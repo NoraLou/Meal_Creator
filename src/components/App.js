452378fc28710ac3e8c271748467ba9c23988226
Nora Humpage <nora.humpage@gmail.com>
2017-08-18 11:30:19 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default App
